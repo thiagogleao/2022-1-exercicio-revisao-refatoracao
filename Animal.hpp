ec7b237c32a23e2615bbc5fd9061a91de229ffe2
Thiago Leão <62669258+thiagogleao@users.noreply.github.com>
2022-06-29 13:31:16 -0300

    Update Animal.hpp

--- Animal.hpp
+++ Animal.hpp
@@ -4,25 +4,14 @@
 #include <iostream>
 #include <iomanip>
 
-using namespace std;
-
-class Animal {
+class Animal 
+{
     public:
-        string IDADE;
-        string family;
-        string nome;
+        std::string idade;
+        std::string familia;
+        std::string nome;
 
-        void print() {
-            cout << "\n[Animal]" << endl
-            << "  Nome: " << nome << endl
-
-              << "  Idade: " << IDADE << endl
-            << "  Familia: " << family << endl;
-        }
-
-        void print_oi(){
-            cout << "Tchau" << endl;
-        }
+        virtual void print();
 };
 
 #endif
