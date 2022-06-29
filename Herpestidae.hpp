e03401a52675cd7ba639af44c237f374f7a0ae2f
Thiago Leão <62669258+thiagogleao@users.noreply.github.com>
2022-06-29 13:42:30 -0300

    Update Herpestidae.hpp

--- Herpestidae.hpp
+++ Herpestidae.hpp
@@ -1,35 +1,14 @@
-#ifndef Herpestidae_HPP
-#define Herpestidae_HPP
+#ifndef HERPESTIDAE_HPP
+#define HERPESTIDAE_HPP
 
 #include "Animal.hpp"
 
-using namespace std;
-
-double quantPORCAO = 1;
-
-
-class Herpestidae : public Animal {
-
+class Herpestidae : public Animal 
+{
     public:
-
-    public:
-  string species;
-
-    double kgConsumidosDeRacao(int porcoesConsumidas) {
-        double c = quantPORCAO*porcoesConsumidas;
-                  return c;
-    }
-
-
-    void print() {
-
-        Animal::print();
-
-
-
-        std::cout << "  Especie: " << species <<endl;
-
-    }
+    std::string especie;
+    double kgConsumidosDeRacao(int porcoesConsumidas);
+    void print() override;
 };
 
 #endif
