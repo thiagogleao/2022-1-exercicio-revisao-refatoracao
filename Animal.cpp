3d28352874f7ca9a83e45ae7509172149b7b11a6
Thiago Leão <62669258+thiagogleao@users.noreply.github.com>
2022-06-29 13:34:23 -0300

    Create Animal.cpp

---
+++ Animal.cpp
@@ -0,0 +1,9 @@
+#include "Animal.hpp"
+
+void Animal::print()
+{
+    std::cout << std::endl << "[Animal]" << std:: endl;
+    std::cout << "Nome: " << nome << std:: endl;
+    std::cout << "Idade: " << idade << std:: endl;
+    std::cout << "Familia: " << familia << std::endl;
+ }
